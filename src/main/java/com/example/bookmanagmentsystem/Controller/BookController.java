fa6bb1d4b70a47678d901f113b87cbeb805d3a85
kshitij-24-cyber <68536909+kshitij-24-cyber@users.noreply.github.com>
2022-05-23 11:40:26 +0530

    added DeleteMapping Request

--- src/main/java/com/example/bookmanagmentsystem/Controller/BookController.java
+++ src/main/java/com/example/bookmanagmentsystem/Controller/BookController.java
@@ -7,7 +7,6 @@ package com.example.bookmanagmentsystem.Controller;/*
 import com.example.bookmanagmentsystem.Model.Book;
 import com.example.bookmanagmentsystem.Service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
